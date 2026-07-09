90401b1740a6f5f9c11ffcb48382c54c30898b4b
as1291 <amrit.tiwari2002@gmail.com>
2026-07-09 21:08:27 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,20 @@ int main()
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    if(input=="exit") break;
-    else if(input.substr(0,5)=="echo ") std::cout<<input.substr(5) << std::endl;
+    std::string command = input.substr(0,input.find(' '));
+    std::string param;
+
+    const std::size_t paramIndex = input.find(' ')+1;
+    if(paramIndex!=std::string::npos){
+      param=input.substr(paramIndex);
+    }
+    if(command=="exit") break;
+    else if(command=="type "){
+      if(param=="echo" || param=="exit" || param=="type") {
+        std::cout<<param<< " is a shell builtin" << std::endl;
+      }
+    }
+    else if(command=="echo ") std::cout<<param << std::endl;
     else std::cout << input << ": command not found" << std::endl;
   }
 }
