42e71fb314d6931e0abdc4e00720493fce3417b3
as1291 <amrit.tiwari2002@gmail.com>
2026-07-09 14:53:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main()
     std::string input;
     std::getline(std::cin, input);
     if(input=="exit") break;
-    else if(input.substr(0,5)=="echo") std::cout<<input.substr(5) << std::endl;
-    std::cout << input << ": command not found" << std::endl;
+    else if(input.substr(0,5)=="echo ") std::cout<<input.substr(5) << std::endl;
+    else std::cout << input << ": command not found" << std::endl;
   }
 }
