0495eb072bda1c1927fd70a1f6b8a18ad5f396db
as1291 <amrit.tiwari2002@gmail.com>
2026-07-10 23:36:23 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,66 @@
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 #include <string>
+#include <unistd.h>
+#include <vector>
+
+std::vector<std::string> commands = {"exit", "echo", "type"};
+
+void callType(std::string param)
+{
+  auto it = std::find(std::begin(commands), std::end(commands), param);
+  if (it != std::end(commands))
+    std::cout << param << " is a shell builtin" << std::endl;
+  else
+  {
+    std::string pathvar = std::getenv("PATH");
+    std::istringstream path_stream(pathvar);
+    std::string pathsplit;
+    while (std::getline(path_stream, pathsplit, ':'))
+    {
+      std::string filepath = pathsplit + '/' + param;
+      if (access(filepath.c_str(), X_OK) == 0)
+      {
+        std::cout << param << " is " << filepath << std::endl;
+        return;
+      }
+    }
+    std::cout << param << ": not found" << std::endl;
+  }
+}
+
+void callEcho(std::string param)
+{
+  std::cout << param << std::endl;
+}
+
+int REP()
+{
+  std::cout << "$ ";
+  std::string input;
+  std::getline(std::cin, input);
+  if (input == "exit")
+    return -1;
+
+  std::string command = input.substr(0, input.find(' '));
+  std::size_t paramIndex = input.find(' ');
+  std::string param;
+  if (paramIndex != std::string::npos)
+    param = input.substr(paramIndex + 1);
+
+  if (command == "exit")
+    return -1;
+  else if (command == "echo")
+    callEcho(param);
+  else if (command == "type")
+    callType(param);
+  else
+    std::cout << input << ": command not found" << std::endl;
+  return 0;
+}
 
 int main()
 {
@@ -7,31 +68,13 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  while (true)
+  while (1)
   {
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    std::string command = input.substr(0,input.find(' '));
-    std::string param;
-
-    const std::size_t paramIndex = input.find(' ')+1;
-    if(paramIndex!=std::string::npos){
-      param=input.substr(paramIndex);
+    int returnval;
+    returnval = REP();
+    if (returnval == -1)
+    {
+      break;
     }
-
-    if(command=="exit") break;
-    else if(command=="type"){
-      if(param=="echo" || param=="exit" || param=="type") {
-        std::cout<<param<< " is a shell builtin" << std::endl;
-      }else
-      {
-        std::cout << param << ": not found" << std::endl;
-      }
-      
-    }
-    else if(command=="echo") std::cout<<param << std::endl;
-    else std::cout << input << ": command not found" << std::endl;
   }
 }
