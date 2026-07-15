d52b5a82207d88426e557708fa28a5af7ef67a0e
as1291 <amrit.tiwari2002@gmail.com>
2026-07-15 16:36:56 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,7 @@ void callCd(std::string path)
   // if(path.empty())
   // std::cout<<"empty file name"
   if(chdir(path.c_str())!=0){
-    std::cout << "cd: " << argument << ": No such file or directory" << std::endl;
+    std::cout << "cd: " << param << ": No such file or directory" << std::endl;
   }
 }
 
